bf3a976a84a733f15af9318fb7d72a1aa1aa71d2
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2024-01-13 01:46:40 -0300

    add push to master

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,8 +11,20 @@ import (
 )
 
 type GithubWebhook struct {
+	Ref string `json:"ref"`
+	Pusher Pusher `json:"pusher"`
+	HeadCommit HeadCommit `json:"head_commit"`
 	Action string `json:"action"`
    	PullRequest PullRequest `json:"pull_request"`
+}
+
+type HeadCommit struct {
+	Id string `json:"id"`
+	Message string `json:"message"`
+}
+type Pusher struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
 }
 type MergedBy struct {
     Login string `json:"login"`
@@ -55,13 +67,37 @@ func (s *Server) RegisterRoutes() http.Handler {
 }
 
 func (s *Server) GithubWebhookHandler(c echo.Context) error {
+
 	webhook := new(GithubWebhook)
     if err := c.Bind(webhook); err != nil {
         return err
     }
+	
+	pushDirectlyToMaster := webhook.Ref == "refs/heads/master"
+	pullRequestMerged := webhook.Action == "closed" && webhook.PullRequest.Merged && webhook.PullRequest.Base.Ref == "master"
 
+	if(pushDirectlyToMaster){
+		fmt.Println("pusher name", webhook.Pusher.Name)
+		fmt.Println("pusher email", webhook.Pusher.Email)
+		fmt.Println("pusher head commit id", webhook.HeadCommit.Id)
+		fmt.Println("pusher head commit message", webhook.HeadCommit.Message)
 
-	if(webhook.Action == "closed" && webhook.PullRequest.Merged && webhook.PullRequest.Base.Ref == "master"){
+		err := sshclient.UpdateRepository()
+
+		if err != nil {
+			slog.Error("Error update repository")
+			return c.JSON(http.StatusInternalServerError, map[string]string{
+				"message": "error update repository",
+			})
+		}
+
+		slog.Info("Repository updated")
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "update repository",
+		})
+	}
+
+	if(pullRequestMerged){
 		fmt.Println("pull merged", webhook.PullRequest.Merged)
 		fmt.Println("pull merged at", webhook.PullRequest.MergedAt)
 		fmt.Println("pull merged by", webhook.PullRequest.MergedBy.Login)
