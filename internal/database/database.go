0b674b0092b8e7736f21fdb558391e4da2aa876d
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2024-01-20 13:22:26 -0300

    add migrations

--- internal/database/database.go
+++ internal/database/database.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	_ "github.com/joho/godotenv/autoload"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type Service interface {
@@ -25,12 +25,32 @@ var (
 )
 
 func New() Service {
-	db, err := sql.Open("sqlite3", dburl)
+	db, err := sql.Open("libsql", dburl)
 	if err != nil {
 		// This will not be a connection error, but a DSN parse error or
 		// another initialization error.
 		log.Fatal(err)
 	}
+
+	fmt.Println("dburl", dburl)
+
+
+	
+	_,migration_err := db.Exec(`CREATE TABLE IF NOT EXISTS "updates" (
+		ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+		"pusher_name" TEXT, 
+		"branch" TEXT, 
+		"status" TEXT, 
+		"created_at" DATETIME DEFAULT CURRENT_TIMESTAMP, 
+		"updated_at" DATETIME DEFAULT CURRENT_TIMESTAMP
+	)`)
+
+	if migration_err != nil {
+		error := fmt.Sprintf("migration_err: %v", migration_err)
+		fmt.Println(error)	
+		log.Fatal(migration_err)
+	}
+
 	s := &service{db: db}
 	return s
 }
